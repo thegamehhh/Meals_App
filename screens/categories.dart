59df001d5359965550fd467767979f79de0c2d45
thegamehhh <thegamehhh987@gmail.com>
2023-09-16 11:41:38 +0530

    meal_detail

--- screens/categories.dart
+++ screens/categories.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:meals/data/dummy_data.dart';
-import 'package:meals/models/category.dart';
-import 'package:meals/screens/meals.dart';
-import 'package:meals/widgets/category_grid_item.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
--- screens/meals.dart
+++ screens/meals.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:meals/models/meal.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/meal_detail.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
   final String title;
   final List<Meal> meals;
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailScreen(meal: meal),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget content = Center(
@@ -34,7 +45,11 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
           itemCount: meals.length,
-          itemBuilder: (ctx, index) => Text(meals[index].title));
+          itemBuilder: (ctx, index) => MealItem(
+              meal: meals[index],
+              onSelectMeal: (meal) {
+                selectMeal(context, meal);
+              }));
     }
     return Scaffold(appBar: AppBar(title: Text(title)), body: content);
   }
